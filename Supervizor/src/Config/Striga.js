b91643abc55a4a6d115d239a671465e68e0029e7
Emir6831 <80280480+Emir6831@users.noreply.github.com>
2021-07-11 15:22:46 +0300

    Add files via upload

---
+++ Supervizor/src/Config/Striga.js
@@ -0,0 +1,15 @@
+const striga = {
+serverID: "843446241839808544",//Sunucu idsi kanks
+token: "ODYzNDUyNTk4OTcwODc1OTI0.YOnG5Q.U1oxofm3Fr_5NnNdtW1Ob0oERGI",//Botun tokeni kanks
+ownerID: "544525045884649475", //Bot sahibinin idsi knaks
+prefix: ["s!", "."], //prefixler "!ceza bilgi 20" falan fıstık kanks
+voiceID: "861682965480996934", //Botun hangi ses kanalına girecekse o ses kanalının IDsi kanks
+
+sunucuTag: "Saé",//Sunucunun Tagı İle Takılan Üyelerin Tagları Anladın Kanks yani sunucunun tagı kanks
+tagsızTag: "`",//Sunucunun Tagsız Takılan Üyelerinin Tagları Anladın kanks
+
+statusText: "deep <3 saé", //Kafana göre kanks
+status: "dnd" //Botun çevirim içi kısmı kanks - dnd-online-idle-invisible
+} 
+
+module.exports = striga
